930ce6284ada6ff569e9d01489ebaf76c0a51f09
MD Moniruzzaman Arafat <arafatmonir091@gmail.com>
2022-06-20 12:40:20 +0600

    chapter two hello.js update

--- chapter_two/hello.js
+++ chapter_two/hello.js
@@ -1,9 +1,9 @@
 console.log('hello world'); //normal string print with single quotation
 console.log("i love bangladesh"); //normal string print with double quotation
-console.log(40);
-console.log(40.23);
-console.log('my roll number is ' + 10);
-console.log(9 + 9);
-console.log('9' + 9);
-console.log('hello ' + 'world');
+console.log(40); //normal integer number print
+console.log(40.23); //normal float number print
+console.log('my roll number is ' + 10); // concat this number + string
+console.log(9 + 9); // normal addition
+console.log('9' + 9); // concat this number + string
+console.log('hello ' + 'world'); // concat this string + string
 
